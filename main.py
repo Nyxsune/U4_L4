022f28ac8659ad5b83d74aad530e9591a1c2ac90
Connor Cox <ccox25@cpatech.org>
2024-12-11 19:05:53 +0000

    final

--- main.py
+++ main.py
@@ -1,27 +1,117 @@
 """
 Connor Cox
-U4 Lab 2
-Reversal
+U4 Lab 4
+balanced brackets
 """
 from StackClass import ArrayStack
 
-def reverse(sentence):
-    original = list(sentence)
+def first_check(line):
+    line = list(line)
     stack = ArrayStack()
-    for char in original:
-        stack.push(char)
-    
-    new = ""
-    for i in range(len(stack)):
-        new += stack.pop()
-    
-    return new
+    brackets = {
+        "curly":0,
+        "square":0,
+        "normal":0
+    }
+    isBalanced = False
 
+    for item in line:
+        if item == "{" or item == "}":
+            brackets["curly"] += 1
+        elif item == "[" or item == "]":
+            brackets["square"] += 1
+        elif item == "(" or item == ")":
+            brackets["normal"] += 1
+        stack.push(item)
+    
+    if brackets["curly"]%2 == 0 and brackets["square"]%2 == 0 and brackets["normal"]%2 == 0:
+        isBalanced = True
+
+    return isBalanced, stack
+
+def second_check(stack):
+    #I know it looks quirky, but I was literally about to have a breakdown over making this work, so this is what you get
+    normNum = 0
+    curlNum = 0
+    sqrNum = 0
+    index = 0
+    brackets = {}
+    for i in range(len(stack)):
+        bracket = stack.pop()
+        if bracket == "]":
+            sqrNum += 1
+            key = "Square" + str(sqrNum)
+            brackets[key] = index
+
+        elif bracket == ")":
+            normNum += 1
+            key = "Normal" + str(normNum)
+            brackets[key] = index
+
+        elif bracket == "}":
+            curlNum += 1
+            key = "Curly" + str(curlNum)
+            brackets[key] = index
+        
+        elif bracket == "[":
+            key = "Square" + str(sqrNum)
+            distance = index - brackets[key]
+            del brackets[key]
+            if distance % 2 == 0:
+                isBalanced = False
+                break
+            else:
+                isBalanced = True
+            sqrNum -= 1
+
+        elif bracket == "(":
+            key = "Normal" + str(normNum)
+            distance = index - brackets[key]
+            del brackets[key]
+            if distance % 2 == 0:
+                isBalanced = False
+                break
+            else:
+                isBalanced = True
+            normNum -= 1
+
+        elif bracket == "{":
+            key = "Curly" + str(curlNum)
+            distance = index - brackets[key]
+            del brackets[key]
+            if distance % 2 == 0:
+                isBalanced = False
+                break
+            else:
+                isBalanced = True
+            curlNum -= 1
+        index += 1
+    
+    return isBalanced
+
+def balanced(line):
+    isBalanced, stack = first_check(line)
+
+    if isBalanced:
+        isBalanced = second_check(stack)
+
+    return isBalanced
 
 def main():
-    original = "Sphinx of black quartz, judge my vow"
-    reversed = reverse(original)
-    print(f"Original: {original}\nReversed: {reversed}")
+    test1 = "()(()){([()])}"
+    test2 = "((()(()){([()])}))"
+    test3 = ")(()){([()])]"
+    test4 = "({[])}"
+    test5 = "("
+
+    for test in [test1, test2, test3, test4, test5]:
+        isBalanced = balanced(test)
+        if isBalanced:
+            isBalanced = "Balanced"
+        else:
+            isBalanced = "Unbalanced"
+        print(f"{test} - {isBalanced}")
+
 
 if __name__ == "__main__":
     main()
